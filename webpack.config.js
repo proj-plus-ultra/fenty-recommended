f0e90992e574c0fe688e39d1c9ec81c63d031e1d
Michael Siu <michaeltsiu13@gmail.com>
2020-05-13 12:45:14 -0700

    Fixed syntax errors via ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
   },
   module: {
     rules: [
@@ -17,20 +18,20 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins:['@babel/plugin-proposal-class-properties']
-        }
+          plugins: ['@babel/plugin-proposal-class-properties'],
+        },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
-    {
-      test: /\.(eot|woff|woff2|gif|svg|ttf)([\?]?.*)$/,
-      use: ['file-loader']
-    }
-    ]
+      {
+        test: /\.(eot|woff|woff2|gif|svg|ttf)([?]?.*)$/,
+        use: ['file-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
 };
